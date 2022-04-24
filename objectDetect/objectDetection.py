2ebb78f747ab736ea98c21bbae4198ddf66864f5
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-25 03:38:51 +0700

    Update objectDetection.py

--- objectDetect/objectDetection.py
+++ objectDetect/objectDetection.py
@@ -8,3 +8,7 @@ while True:
 
     success, frame = cap.read()
     height, width, _ = frame.shape
+    
+    masking = objectDetect.apply(frame)
+    _, masking = cv2.threshold(masking, 254, 255, cv2.THRESH_BINARY)
+    countours, _ = cv2.findContours(masking, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)

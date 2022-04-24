59ea4fba54692d8841e8edb3d7e49f388c8cb32b
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-24 23:05:07 +0700

    Update code.py

--- objectDetect/code.py
+++ objectDetect/code.py
@@ -1,3 +1,10 @@
 import cv2
 
 cap = cv2.VideoCapture("highway.mp4")
+
+objectDetect = cv2.createBackgroundSubtractorMOG2()
+
+while True:
+
+    success, frame = cap.read()
+    height, width, _ = frame.shape

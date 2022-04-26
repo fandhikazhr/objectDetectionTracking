66dc3d7d378ede696248f207ceaa4d4d13f91761
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-27 04:06:21 +0700

    Update track.py

--- objectTrack/track.py
+++ objectTrack/track.py
@@ -7,3 +7,8 @@ tracked = cv2.TrackerMOSSE_create()
 success, img = cap.read()
 tracker = cv2.selectROI("Track", img, False)
 tracked.init(img,tracker)
+
+def startTrack(img, tracker):
+    x, y, w, h = int(tracker[0]),int(tracker[1]),int(tracker[2]),int(tracker[3])
+    cv2.rectangle(img, (x,y), ((x + w),(y+h)), (0,0,255), 3, 1)
+    cv2.putText(img, "Tracking", (75,75), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 3)

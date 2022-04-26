d17f9b452adae805170351f8832b823cbf1cf31c
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-27 04:05:39 +0700

    Update track.py

--- objectTrack/track.py
+++ objectTrack/track.py
@@ -1,3 +1,9 @@
 import cv2
 
 cap = cv2.VideoCapture("object.mp4")
+
+tracked = cv2.TrackerMOSSE_create()
+# tracked = cv2.TrackerCSRT_create() # slow fps but precision
+success, img = cap.read()
+tracker = cv2.selectROI("Track", img, False)
+tracked.init(img,tracker)
